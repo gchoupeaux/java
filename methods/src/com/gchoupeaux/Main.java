533717031f34a360af489308b911cdcf4727c06d
Guillaume Choupeaux <guillaume_choupeaux@apple.com>
2018-03-15 19:27:18 -0700

    methods return

--- methods/src/com/gchoupeaux/Main.java
+++ methods/src/com/gchoupeaux/Main.java
@@ -3,18 +3,52 @@ package com.gchoupeaux;
 public class Main {
 
     public static void main(String[] args) {
-        calculateScore(true, 800, 5, 100);
-        calculateScore(true, 1000, 5, 200);
+
+        int highScore = calculateScore(true, 800, 5, 100);
+
+
+        displayHighScorePosition( "Guillaume", calculateHighScorePosition(highScore));
+
+        // System.out.println("Your final score was " + highScore);
+
+        highScore = calculateScore(true, 1000, 5, 200);
+
+        // System.out.println("Your final score was " + highScore);
+
+        displayHighScorePosition( "Guillaume", calculateHighScorePosition(highScore));
+
+        displayHighScorePosition( "Joe", calculateHighScorePosition(1000));
     }
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
         if(gameOver) {
-            finalScore = score + (levelCompleted * bonus);
+            int finalScore = score + (levelCompleted * bonus);
             finalScore += 1000;
-            System.out.println("Your final score was " + finalScore);
             return finalScore;
         }
         return -1;
+    }
+
+    public static void displayHighScorePosition(String playerName, int position) {
+
+        System.out.println(playerName + " managed to get into position " + position + " on the high score table");
+    }
+
+    public static int calculateHighScorePosition(int playerScore) {
+
+        int position = 0;
+
+        if (playerScore >= 1000) {
+            position = 1;
+        } else if (playerScore < 1000 && playerScore >= 500 ){
+            position = 2;
+        } else if (playerScore < 500 && playerScore >= 100 ){
+            position = 3;
+        } else {
+            position = 4;
+        }
+
+        return position;
     }
 }
 
